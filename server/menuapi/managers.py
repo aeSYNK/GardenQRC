b03e9bf938794989484335abd535df2dd5167857
aeSynk <danik_2198@mail.ru>
2022-02-05 19:38:38 +0600

    222

--- server/menuapi/managers.py
+++ server/menuapi/managers.py
@@ -13,25 +13,27 @@ class UserManager(BaseUserManager):
     для создания объектов `User`.
     """
 
-    def _create_user(self, username, password=None, **extra_fields):
+    def _create_user(self, username, password, **extra_fields):
         if not username:
             raise ValueError('Указанное имя пользователя должно быть установлено')
 
         user = self.model(username=username, **extra_fields)
-        # print(password)
-        password = BaseUserManager().make_random_password(4, digits)
-        print(password)
+        print(f'Первая проверка _create_user: {password}')
+        if password == None:
+            password = BaseUserManager().make_random_password(4, digits)
+        print(f'Вторая проверка _create_user: {password}')
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
     # def create_user(self, username, password=None, **extra_fields):
-    def create_user(self, username, password=None, **extra_fields):
+    def create_user(self, username, password, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
 
         return self._create_user(username, password, **extra_fields)
+
 
     def create_superuser(self, username, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
